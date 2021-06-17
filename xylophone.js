a69ce7f3953ac3f89b2545e61a8e8e3c4935bf98
Lev <levdevuk@gmail.com>
2021-06-17 18:19:54 +0100

    Fixed event delegation bug and added subtle animation

--- xylophone.js
+++ xylophone.js
@@ -2,6 +2,9 @@ const xylo = document.querySelector('.xylo')
 let wordNum = 1
 
 xylo.addEventListener('click', e => {
-  new Audio(`sounds/${wordNum}_${e.target.id}.mp3`).play()
-  wordNum < 5 ? (wordNum += 1) : (wordNum = 1)
+  btn = e.target.closest('button')
+  if (btn) {
+    new Audio(`sounds/${wordNum}_${btn.id}.mp3`).play()
+    wordNum < 5 ? (wordNum += 1) : (wordNum = 1)
+  }
 })
