8c7dc4f2000f6d9acd18e90f0baf83298a381bf3
Lev <levdevuk@gmail.com>
2024-10-18 18:03:42 +0100

    Simplified
    - Added guard clause
    - Improved colours

--- xylophone.js
+++ xylophone.js
@@ -1,10 +1,8 @@
-const xylo = document.querySelector('.xylo')
 let wordNum = 1
 
-xylo.addEventListener('click', e => {
-  btn = e.target.closest('button')
-  if (btn) {
-    new Audio(`sounds/${wordNum}_${btn.id}.mp3`).play()
-    wordNum < 5 ? (wordNum += 1) : (wordNum = 1)
-  }
+document.querySelector('.xylo').addEventListener('click', e => {
+  const btn = e.target.closest('button')
+  if (!btn) return
+  new Audio(`sounds/${wordNum}_${btn.id}.mp3`).play()
+  wordNum < 5 ? (wordNum += 1) : (wordNum = 1)
 })
